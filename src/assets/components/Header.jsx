491e46ae7e74548df9ddf4870ccfcac94487c59e
Ajoy Sarkar (Anondo) <ajoysarker553@gmail.com>
2024-04-20 18:00:00 +0600

    commit

--- src/assets/components/Header.jsx
+++ src/assets/components/Header.jsx
@@ -15,19 +15,31 @@ const Header = () => {
     }, [theme]);
 
     const navlink = <>
-        <li className='hover:bg-yellow-400 px-5 py-2 transition duration-300 ease-in-out rounded-md'>
+        <li className='px-5 py-2 transition duration-300 ease-in-out rounded-md hover:bg-yellow-400'>
             <NavLink to={'/'} className={({ isActive }) => isActive ? 'text-red-500' : 'text-black dark:text-white'}>Home</NavLink>
         </li>
-        <li className='hover:bg-yellow-400 px-5 py-2 transition duration-300 ease-in-out rounded-md'>
+        <li className='px-5 py-2 transition duration-300 ease-in-out rounded-md hover:bg-yellow-400'>
             <NavLink to={'/about'} className={({ isActive }) => isActive ? 'text-red-500' : 'text-black dark:text-white'}>About</NavLink>
         </li>
-        <li className='hover:bg-yellow-400 px-5 py-2 transition duration-300 ease-in-out rounded-md'>
+        <li className='px-5 py-2 transition duration-300 ease-in-out rounded-md hover:bg-yellow-400'>
             <NavLink to={'/blog'} className={({ isActive }) => isActive ? 'text-red-500' : 'text-black dark:text-white'}>Blog</NavLink>
         </li>
-        <li className='hover:bg-yellow-400 px-5 py-2 transition duration-300 ease-in-out rounded-md'>
+        <li className='px-5 py-2 transition duration-300 ease-in-out rounded-md hover:bg-yellow-400'>
             <NavLink to={'/login'} className={({ isActive }) => isActive ? 'text-red-500' : 'text-black dark:text-white'}>Login</NavLink>
         </li>
     </>;
+
+
+
+
+
+
+
+
+
+
+
+
 
     return (
         <div className="navbar bg-base-100 dark:bg-gray-900 dark:text-white">
@@ -36,7 +48,7 @@ const Header = () => {
                     <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
                         <svg
                             xmlns="http://www.w3.org/2000/svg"
-                            className="h-5 w-5"
+                            className="w-5 h-5"
                             fill="none"
                             viewBox="0 0 24 24"
                             stroke="currentColor">
@@ -53,17 +65,17 @@ const Header = () => {
                         {navlink}
                     </ul>
                 </div>
-                <a className="btn btn-ghost text-xl">daisyUI</a>
+                <a className="text-xl btn btn-ghost">daisyUI</a>
             </div>
-            <div className="navbar-center hidden lg:flex">
-                <ul className="gap-6 menu-horizontal px-1">
+            <div className="hidden navbar-center lg:flex">
+                <ul className="gap-6 px-1 menu-horizontal">
                     {navlink}
                 </ul>
             </div>
-            <div className="navbar-end flex items-center gap-4">
+            <div className="flex items-center gap-4 navbar-end">
                 <button
                     onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
-                    className="p-2 rounded-full bg-gray-200 dark:bg-gray-700 transition-all"
+                    className="p-2 transition-all bg-gray-200 rounded-full dark:bg-gray-700"
                 >
                     {theme === "dark" ? <Sun size={20} className="text-yellow-400" /> : <Moon size={20} />}
                 </button>
